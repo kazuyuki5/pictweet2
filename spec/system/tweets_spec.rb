bfce684af78b1874218a10431fd5707c929fbb5d
kazuyuki5 <kazuyuki2957@gmail.com>
2021-01-13 08:07:44 +0900

    テストコードの修正

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'ツイート投稿', type: :system do
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
       find('input[name="commit"]').click
-      expect(current_path).to habe_content('投稿する')
+      expect(current_path).to eq root_path
       # 新規投稿ページへのリンクがあることを確認する
       expect(page).to have_content('投稿する')
       # 投稿ページに移動する
@@ -32,7 +32,7 @@ RSpec.describe 'ツイート投稿', type: :system do
       # トップページに遷移する
       visit root_path
       # トップページには先ほど投稿した内容のツイートが存在することを確認する(画像)
-      expect(page).to have_selector ".content_post[style='background-image: url#{@tweet_image});']"
+      expect(page).to have_selector ".content_post[style='background-image: url(#{@tweet_image});']"
       # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
       expect(page).to have_content(@tweet_text)
     end
@@ -43,7 +43,7 @@ RSpec.describe 'ツイート投稿', type: :system do
       visit root_path
       # 新規投稿ページへのリンクがない
       expect(page).to have_no_content('投稿する')
-    endt
+    end
   end
 end
 
@@ -77,9 +77,9 @@ RSpec.describe 'ツイート編集', type: :system do
       fill_in 'tweet_image', with: "#{@tweet1.image}+編集した画像URL"
       fill_in 'tweet_text', with: "#{@tweet1.text}+編集したテキスト"
       # 編集してもTweetモデルのカウントは変わらないことを確認する
-      expect(
+      expect{
         find('input[name="commit"]').click
-      ).to change { Tweet.count }.by(0)
+      }.to change { Tweet.count }.by(0)
       # 編集完了画面に遷移したことを確認する
       expect(current_path).to eq tweet_path(@tweet1)
       # 「更新が完了しました」の文字があることを確認する
@@ -87,7 +87,7 @@ RSpec.describe 'ツイート編集', type: :system do
       # トップページに遷移する
       visit root_path
       # トップページには先ほど変更した内容のツイートが存在することを確認する（画像）
-      expect(page).to have_selector  ".content_post[style='background-image: url(#{@tweet1.image}+編集した画像URL）;']"
+      expect(page).to have_selector  ".content_post[style='background-image: url(#{@tweet1.image}+編集した画像URL);']"
       # トップページには先ほど変更した内容のツイートが存在することを確認する（テキスト）
       expect(page).to have_content("#{@tweet1.text}+編集したテキスト")
     end
@@ -99,7 +99,7 @@ RSpec.describe 'ツイート編集', type: :system do
       fill_in 'Email', with: @tweet1.user.email
       fill_in 'Password', with: @tweet1.user.password
       find('input[name="commit"]').click
-      expecct(current_path).to eq root_path
+      expect(current_path).to eq root_path
       # ツイート2に「編集」ボタンがないことを確認する
       expect(
         all(".more")[0].hover
@@ -107,7 +107,7 @@ RSpec.describe 'ツイート編集', type: :system do
     end
     it 'ログインしていないとツイートの編集画面には遷移できない' do
       # トップページにいる
-      visti root_path
+      visit root_path
       # ツイート1に「編集」ボタンがないことを確認する
       expect(
         all(".more")[1].hover
